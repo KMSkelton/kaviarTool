1f22c93b8ac3310c0a0a28a644b3417ce6701464
KMSkelton <kmskelton@outlook.com>
2016-04-01 13:14:52 -0700

    jQuery table works

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-
+$("#table_id").hide();
   $("form").submit(function(event) {
       event.preventDefault();
       var kaviarData = {
@@ -13,33 +13,75 @@ $(document).ready(function() {
         "variant_type": $("select[name=variant_type]").val()
       };
 console.log(kaviarData);
-// $('#table_id').DataTable({
-//     ajax: {
 $.ajax({
       type: "POST",
       url: "http://localhost/~kristopherskelton/kaviarTool/cgi-bin/Kaviar",
       data: kaviarData,
       // dataType: "json",
       encode: true,
-      // dataSrc: "sites",
  //      error: function (xhr) {
  //   alert(JSON.stringify(xhr));
  // },
  beforeSend: function(jqXHR, settings) {
    console.log(settings.url+ '?' + settings.data);
+ },
+ success: $("#table_id").show(),
+ success: function(data, textStatus, jqXHR){
+  //  console.log("woot - I'm in success!");
+  //  console.log(data);
+  //  console.log(data.sites[0]["chromosome"]);
+   $.each(data.sites, function(index, element){
+     var rsid = data.sites[index]["rsids"];
+     if (rsid === undefined){
+       data.sites[index]["rsids"] = ".";
+       rsid = data.sites[index]["rsids"];
+     } else {
+       rsid = element["rsids"][0];
+     };
+    //  console.log(rsid + " = rsid");
+    //  console.log(element);
+    //  console.log("data.sites", data.sites);
+    //  console.log("element['varInfo']", element["varInfo"]);
+     var infoCell = [];
+     $.each(data.sites[index]["varInfo"], function(infoIndex, infoElement){
+       console.log(data.sites);
+       if (infoIndex > 0){
+         console.log("I'm in the if! The next TR should have a class.");
+         infoCell.push("<tr><td>" + infoElement["variant"] + "(" + infoElement["frequency"] + ")" + "</td><td>" + infoElement["sources"] + "</td></tr></tr>");
+       } else {
+       infoCell.push("<td>" + infoElement["variant"] + "(" + infoElement["frequency"] + ")" + "</td><td>" + infoElement["sources"] + "</td></tr>");
+       console.log("infoIndex: ", infoIndex);
+       console.log("infoElement", infoElement["variant"]);
+       console.log("infoCell", infoCell);
+     }
+     })
+     console.log(infoCell);
+     var rowSpan = infoCell.length;
+     var row = "<tr><td rowspan=" + rowSpan + ">" +
+     element["chromosome"] +
+     " " +
+     element["position"] +
+     "</td><td rowspan=" + rowSpan + ">" +
+     rsid +
+     "</td>" +
+     infoCell;
+     $("#tableBody").append(row);
+   });
+ },
+ error: function(jqXHR, textStatus, errorThrown){
+console.log(jqXHR + "  jqXHR in error");
+console.log(textStatus + "  textStatus in error");
+console.log(errorThrown + "  errorThrown in error");
  }
-    // },
-  //   columns: [
-  //      { data: 'chromosome' },
-  //      { data: 'position' }
-  // ]
-})
-    .done(function() {
-      console.log("done!!");
-    })
-    .fail(function(jqXHR){
-      console.log(jqXHR);
-    })
-    console.log("flew past the done method...");
-  });
+});
+  //   .done(function(xhr) {
+  //     console.log("done!!");
+  //     console.log(xhr + "  xhr in done method");
+  //   })
+  //   .fail(function(jqXHR){
+  //     console.log(jqXHR + " the failzor");
+  //   })
+  //   console.log("flew past the done method...");
+  // });
+});
 });

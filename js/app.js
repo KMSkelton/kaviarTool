2589c470738d27f5e02558a9e644c83f852d6c4c
denise <denise@galbi.local>
2016-04-06 00:45:56 -0700

    List and file upload fields work correctly. HTML message for empty results caused by MAF filtering.

--- js/app.js
+++ js/app.js
@@ -6,30 +6,67 @@ $("#table_id").hide();
         "frz": $("select[name=frz]").val(),
         "onebased": $("select[name=onebased]").val(),
         "onebased_output": $("select[name=onebased_output]").val(),
-        "chr": $("input[name=chr]").val(),
-        "pos": $("input[name=pos]").val(),
-        "min_maf": $("input[name=min_maf]").val(),
-        "max_maf": $("input[name=max_maf]").val(),
         "variant_type": $("select[name=variant_type]").val()
       };
+      var minMaf = $("input[name=min_maf]").val();
+      if (minMaf) {
+        kaviarData["min_maf"] = minMaf;
+      }
+      var maxMaf = $("input[name=max_maf]").val();
+      if (maxMaf) {
+	kaviarData["max_maf"] = maxMaf;
+      }
+      var ctType = "application/x-www-form-urlencoded; charset=UTF-8"; 
+      var process = "true";
+      var chromCheck = $("input[name=chr]").val();
+      var posCheck = $("input[name=pos]").val();
+      var listCheck = $("textarea[name=list]").val();
+      var fileCheck = $("input[name=uploaded_file]").val();
+
+      if (chromCheck && posCheck) {
+	kaviarData["chr"] = chromCheck;
+	kaviarData["pos"] = posCheck;
+      }
+      else if (listCheck) {
+	kaviarData["list"] = listCheck;
+      }
+      else if (fileCheck) {
+	kaviarData["uploaded_file"] = fileCheck;
+	var fileInput = $("input[name=uploaded_file]");
+	var file = fileInput.get(0).files[0];
+	console.log(file);
+	kaviarData = new FormData();
+	kaviarData.append('uploaded_file',file);
+	ctType = false;
+	process = false;
+      }	
+
 console.log(kaviarData);
+console.log("contentType "+ctType);
+console.log("processData "+process);
 $.ajax({
       type: "POST",
-      url: "http://localhost/~kristopherskelton/kaviarTool/cgi-bin/Kaviar",
+      url: "http://localhost/~denise/kaviarTool/cgi-bin/Kaviar",
       data: kaviarData,
-      // dataType: "json",
-      encode: true,
- //      error: function (xhr) {
- //   alert(JSON.stringify(xhr));
- // },
- beforeSend: function(jqXHR, settings) {
-   console.log(settings.url+ '?' + settings.data);
- },
- success: $("#table_id").show(),
+      contentType: ctType,
+      processData: process,
+      cache: false,
+      //error: function (xhr) {
+         //alert(JSON.stringify(xhr));
+      //},
+      beforeSend: function(jqXHR, settings) {
+        console.log(settings.url+ '?' + settings.data);
+      },
  success: function(data, textStatus, jqXHR){
-  //  console.log("woot - I'm in success!");
-  //  console.log(data);
-  //  console.log(data.sites[0]["chromosome"]);
+    console.log("woot - I'm in success!");
+    console.log(data);
+    console.log(data.sites.length);
+    if (data.sites.length === 0 ) { // if all results were filtered out by MAF filters
+     $("#filtered").show();
+     $("#filtered").append("<p>All results filtered by MAF filters</p>");
+    } else {
+     $("#table_id").show();
+    console.log(data.sites[0]["chromosome"]);
    $.each(data.sites, function(index, element){
      var rsid = data.sites[index]["rsids"];
      if (rsid === undefined){
@@ -68,6 +105,7 @@ $.ajax({
      infoCell;
      $("#tableBody").append(row);
    });
+   } // end if data
  },
  error: function(jqXHR, textStatus, errorThrown){
 console.log(jqXHR + "  jqXHR in error");
